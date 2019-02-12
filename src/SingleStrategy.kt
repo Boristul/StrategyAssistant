5556cd8af3da665b012db13fd2626e9889be6547
btyulkin <btyulkin@termt.com>
2019-02-13 00:44:14 +0300

    add testing model for single strategy

---
+++ src/SingleStrategy.kt
@@ -0,0 +1,52 @@
+class SingleStrategy (var singleBank: Double, var percent: Double, var currentWinAmount : Double = 0.0,
+                      var winCount: Int = 0, var lossCount: Int = 0, var statusStrat: Boolean = true)
+{
+    fun getWinAmount( bank: Double ) = bank * (percent / 100)
+
+    fun getBidAmount(coefficient : Double, bidAmount: Double) = bidAmount / (coefficient - 1)
+
+    fun getCountWinLine () : Int
+    {
+        var bank = singleBank
+        var count = 0
+
+        while (bank < singleBank * 10)
+        {
+            bank += getWinAmount(bank)
+            count++
+        }
+        return count
+    }
+
+    fun simulateBid(coefficient: Double, status: Boolean) : Boolean
+    {
+        if (currentWinAmount == 0.0)
+            currentWinAmount = getWinAmount(singleBank)
+
+        if (status)
+        {
+            singleBank += currentWinAmount
+            currentWinAmount = 0.0
+            winCount++
+            return true
+        }
+        else
+        {
+            if (getBidAmount(coefficient, currentWinAmount) > singleBank) {
+             println("Loss Strategy")
+                statusStrat = false
+                return false
+            }
+
+            singleBank -= getBidAmount(coefficient, currentWinAmount)
+            currentWinAmount += getBidAmount(coefficient, currentWinAmount)
+            lossCount++
+        }
+        return true
+    }
+
+    fun printStat()
+    {
+        println("Bank: $singleBank, debt: $currentWinAmount, win: $winCount, loss: $lossCount, total: ${winCount + lossCount}")
+    }
+}
--- src/main.kt
+++ src/main.kt
@@ -1,3 +1,34 @@
 fun main(args: Array<String>) {
-    print (2+2)
+
+    var winStratCount = 0
+    var lossStratCount = 0
+
+
+    for (i in 1..100) {
+
+        var st = SingleStrategy(10000.0, 3.0)
+        while (st.singleBank < 100000 && st.statusStrat) {
+            st.simulateBid(2.0, randomBool())
+
+        }
+
+        if (!st.statusStrat) {
+            lossStratCount++
+//        println("you lost!")
+//        st.printStat()
+        } else {
+            winStratCount++
+//        println("you win!")
+//        st.printStat()
+        }
+    }
+
+    println("win: $winStratCount, loss: $lossStratCount")
+
+}
+fun randomBool() : Boolean
+{
+    if ((1..100).random() < 50)
+        return false
+    return true
 }
