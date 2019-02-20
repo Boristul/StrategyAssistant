0d18d33ca8e704a56e83e8b7a976f0758530006a
btyulkin <btyulkin@termt.com>
2019-02-20 14:09:01 +0300

    refactor

--- src/main.kt
+++ src/main.kt
@@ -3,15 +3,14 @@ fun main(args: Array<String>) {
     var winStratCount = 0
     var lossStratCount = 0
 
-    var st = SingleStrategy(10000.0, 3.2)
-
-//    print(st.countOfLoses(2.0))
+    var st = SingleStrategy(10000.0, 10.0)
+   print(st.countOfLoses(3.0))
 
     for (i in 1..100) {
 
-        var st = SingleStrategy(10000.0, 3.0)
+        var st = SingleStrategy(10000.0, 15.0)
         while (st.singleBank < 100000 && st.statusStrat) {
-            st.simulateBid(2.0, randomBool())
+            st.simulateBid(3.0, randomBool())
 
         }
 
